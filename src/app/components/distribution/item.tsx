6597de1b56e17b2f38512c45148f94795a67717a
Afan76781 <94213349+Afan76781@users.noreply.github.com>
2023-11-29 12:37:03 +0500

    Add files via upload

---
+++ src/app/components/distribution/item.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import {
+  Heading,
+  Box,
+  Image,
+  Link,
+  Stack,
+  Icon,
+  useColorModeValue
+} from '@chakra-ui/react'
+import { ReactNode } from 'react';
+import { FiExternalLink } from 'react-icons/fi';
+
+interface Props {
+  title: string;
+  imageSrc: string;
+  downloadLink: string;
+  downloadText?: string;
+  isExternal?: boolean;
+  children: ReactNode
+}
+
+export default function DistributionItem({
+  title,
+  imageSrc,
+  downloadLink,
+  downloadText,
+  isExternal,
+  children
+}: Props) {
+  return (
+    <Stack spacing='3' maxW='280px'>
+      <Image
+        src={imageSrc}
+        alt={title}
+        aspectRatio='1 / 1'
+        width='160px'
+        height='160px'
+      />
+      <Heading as='h3' size='md'>{title}</Heading>
+      <Box>
+        {children}
+      </Box>
+      <Link
+        href={downloadLink}
+        isExternal={isExternal}
+        aria-label={'Download' + title}
+        color={useColorModeValue("brand.800", "brand.400")}
+      >
+        {downloadText || 'Download'} {isExternal && <Icon mx='2px' as={FiExternalLink} />}
+      </Link>
+    </Stack>
+  )
+}
